557c2d449f9fcdc92bdb1eec4b029839d6b6ac30
han928 <a1264029@gmail.com>
2018-09-21 10:25:59 -0700

    test using sunfounder

--- button_control.py
+++ button_control.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import time
+# Set #17 as LED pin
+LedPin = 17
+# Set #18 as button pin
+BtnPin = 18
 
-led_pin = 17
+# Set Led status to True(OFF)
+Led_status = True
 
-button_pin = 18
-
-led_status = True
-
-
+# Define a function to print message at the beginning
 def print_message():
 	print ("========================================")
 	print ("|          Button control LED          |")
@@ -23,47 +26,54 @@ def print_message():
 	print 'Please press Ctrl+C to end the program...'
 	raw_input ("Press Enter to begin\n")
 
-def setup_gpio():
-    GPIO.setmode(GPIO.BCM)
+# Define a setup function for some setup
+def setup():
+	# Set the GPIO modes to BCM Numbering
+	GPIO.setmode(GPIO.BCM)
+	# Set LedPin's mode to output, 
+	# and initial level to high (3.3v)
+	GPIO.setup(LedPin, GPIO.OUT, initial=GPIO.HIGH)
+	# Set BtnPin's mode to input, 
+	# and pull up to high (3.3V)
+	GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	# Set up a falling detect on BtnPin, 
+	# and callback function to swLed
+	GPIO.add_event_detect(BtnPin, GPIO.FALLING, callback=swLed)
 
-    GPIO.setup(led_pin, GPIO.OUT, initial=GPIO.HIGH)
+# Define a callback function for button callback
+def swLed(ev=None):
+	global Led_status
+	# Switch led status(on-->off; off-->on)
+	Led_status = not Led_status
+	GPIO.output(LedPin, Led_status)
+	if Led_status:
+		print 'LED OFF...'
+	else:
+		print '...LED ON'
 
-    GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-    GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=switch_led)
-
-
-def switch_led(env=None):
-    global led_status
-
-    led_status = not led_status
-
-    GPIO.output(led_pin, led_status)
-
-    if led_status:
-        print ("Led off")
-
-    else:
-        print("Led ON")
-
+# Define a main function for main process
 def main():
-    print_message()
+	# Print messages
+	print_message()
+	while True:
+		# Don't do anything.
+		time.sleep(1)
 
-    
-    while True:
-        time.sleep(1)
-
+# Define a destroy function for clean up everything after
+# the script finished 
 def destroy():
+	# Turn off LED
+	GPIO.output(LedPin, GPIO.HIGH)
+	# Release resource
+	GPIO.cleanup()
 
-    GPIO.output(led_pin, GPIO.HIGH)
-    GPIO.cleanup()
+# If run this script directly, do:
+if __name__ == '__main__':
+	setup()
+	try:
+		main()
+	# When 'Ctrl+C' is pressed, the child program 
+	# destroy() will be  executed.
+	except KeyboardInterrupt:
+		destroy()
 
-if __name__ == "__main__":
-    setup_gpio()
-
-    try:
-        main()
-
-    except KeyboardInterrupt:
-        destroy()    
